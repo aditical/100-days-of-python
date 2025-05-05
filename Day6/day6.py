452f6c50077843817fbc166bdafb9dd470ff1321
aditi <alishaacharya77@gmail.com>
2025-05-05 17:58:57 +0545

    day6: rebord maze code

---
+++ Day6/day6.py
@@ -0,0 +1,12 @@
+# reborg maze code 
+# while front_is_clear():
+#     move()
+# turn_left()
+# while not at_goal():
+#     if right_is_clear():
+#         turn_left()
+#         turn_left()
+#         turn_left()
+#         move()
+#     else:
+#         turn_left()
